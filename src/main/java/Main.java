8356d53f604bd9e94483be8a2b9993c1cb4fd57b
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-09-23 13:04:23 +1000

    add adder method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,17 @@
 import java.util.Scanner;
 public class Main {
-    public static void main (String[] args){
+    protected void LinkWrite(ContentList contentList){
+
+    }
+    public static void Main(String args[]) {
         Scanner StandardInput = new Scanner(System.in);
         System.out.println("Enter index page for xbiquge.so:");
         String titleURL = StandardInput.nextLine();
         TitleParser titleParser = new TitleParser();
-        try{
-        titleParser.getPage(titleURL);}
-        catch (Exception e){
-            System.out.println("IDK WTF IS WRONG");
-        }
-        System.out.println("The title is:");
-        System.out.println(titleParser.title);
-        AuthorStructure bookInfo = titleParser.AuthorStructure;
+        titleParser.setURL(titleURL);
+        XmlLoader xmlLoader = new XmlLoader();
+        titleParser.setParamArray(xmlLoader.parseToTitleParamArray("xbiquge.xml"));
+        AuthorStructure bookInfo = titleParser.parseToAuthorStructure();
         System.out.println("Complete reading title info for this book, now parsing links...");
-        LinkParser linkParser = new LinkParser();
-        try{
-        linkParser.getPage(titleURL);}
-        catch (Exception e){
-            System.out.println("IDK WHAT's WRONT WITH THIS EITHER");
-        }
-        System.out.println(linkParser.LinkList);
-        System.out.println("Complete parsing link to array list, now downloading and parsing content");
-        ContentParser contentParser = new ContentParser();
-        contentParser.getContentList(linkParser.LinkList);
     }
 }
--- src/main/java/XmlLoader.java
+++ src/main/java/XmlLoader.java
@@ -1,2 +1,53 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.InputStream;
+
 public class XmlLoader {
+    private Document doc;
+    private DocumentBuilder builder;
+    private String[] ParamArray;
+    private String idTag = "id";
+    private String titleTag ="titletag";
+    private String LinkListid = "listid";
+    private String cssPath = "csspath";
+    private String ignoreAmount = "ignoreamount";
+    public String titleID;
+    public String CSSPath;
+    public String TitleTag;
+    public String[] parseToTitleParamArray(String Path){
+        try {
+            builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        } catch (Exception e){
+            System.out.println("Error configuring XML builder");
+        }
+        NodeList nodeList = doc.getElementsByTagName("parser");
+        Node mainElement = nodeList.item(0);
+        nodeList = mainElement.getChildNodes();
+        mainElement = nodeList.item(0);
+        nodeList = mainElement.getChildNodes();
+        int nodeLength = nodeList.getLength();
+        Node current;
+        for (int i = 0; i<nodeLength;i++){
+            current = nodeList.item(i);
+            if (current.getNodeType() == Node.ELEMENT_NODE){
+                if (current.getNodeName().equals(idTag)){
+                    titleID = current.getTextContent();
+                }
+                else if (current.getNodeName().equals(titleTag)){
+                    titleTag = current.getTextContent();
+                }
+                else if (current.getNodeName().equals(cssPath)){
+                    CSSPath = current.getTextContent();
+                }
+            }
+        }
+        ParamArray[0] = CSSPath;
+        ParamArray[1] = titleID;
+        ParamArray[2] = titleTag;
+        return ParamArray;
+    }
 }
